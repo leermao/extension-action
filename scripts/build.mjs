cfbde0856b7c6a01b2119a32fdf734143c226f22
leermao <447552708@qq.com>
2023-02-08 19:11:31 +0800

    add script

--- scripts/build.mjs
+++
@@ -1,3 +0,0 @@
-import { execSync } from 'child_process';
-
-execSync('web-ext build -s ./extension-firefox/ -n extension-firefox.zip -a ./ -o', { stdio: 'inherit' });
--- tools/firefox.js
+++
@@ -1,2 +0,0 @@
-const jwtIssuer = 'user:17770123:613';
-const jwtSecret = '6444d42fa40f195f0f3b8414a3dce243d4af739e67a87ab05ea33e214d3f5c47';
